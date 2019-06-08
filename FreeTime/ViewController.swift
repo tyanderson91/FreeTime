13c9592bfba8ea254c9ea020f03af9fab0cf7749
Tyler Anderson <tyanderson91@gmail.com>
2019-06-08 10:16:45 -0700

    Initial Commit

---
+++ FreeTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreeTime
+//
+//  Created by Tyler Anderson on 6/8/19.
+//  Copyright © 2019 Tyler Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
